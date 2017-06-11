952c3678bbd39335185915146c9d3ce6c0e759f7
ken1flan <ken1flan@gmail.com>
2017-06-11 22:17:29 +0900

    Add letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :users
   root controller: 'users', action: 'index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
