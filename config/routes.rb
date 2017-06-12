05f4ab0d84f38dec98b36b0387b4e10a37a1a49d
ken1flan <ken1flan@gmail.com>
2017-06-13 08:30:32 +0900

    Add session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :users
   root controller: 'users', action: 'index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
