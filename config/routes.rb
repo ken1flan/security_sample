284a63ca613b6e7a80a14919cc003379d0c59391
ken1flan <ken1flan@gmail.com>
2017-06-19 07:49:39 +0900

    Add csrf sample

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   get 'top/show'
 
   resource :session, only: [:new, :create, :destroy]
+  get '/blogs/create', to: 'blogs#create', as: 'create_blog'
   resources :blogs
   resources :users
   resource :redirector, only: [:show]
-  
+
   root controller: 'top', action: 'show'
 
   if Rails.env.development?
