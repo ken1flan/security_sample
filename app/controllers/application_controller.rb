eba885483b2b7572034f452b59264bd885e7ba61
ken1flan <ken1flan@gmail.com>
2017-06-20 15:42:42 +0900

    Move session helpers to sessions_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :log_in, :log_out, :current_user, :logged_in?
-
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  def logged_in?
-    !current_user.nil?
-  end
+  include SessionsHelper
 end
