f0a0973c952aae0ef41aa49ecf1c9b2cd7e5b6d0
ken1flan <ken1flan@gmail.com>
2017-06-22 19:47:06 +0900

    Add blog search

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,22 @@
 class BlogsController < ApplicationController
   def index
-    @blogs = Blog.published.order("updated_at DESC")
+    @blogs = Blog.includes(:user).published.order("updated_at DESC")
+
+    @search_text = params[:search_text]
+    @search_start_at = params[:search_start_at]
+    @search_end_at = params[:search_end_at]
+
+    if @search_text.present?
+      @blogs = @blogs.where("title LIKE \"%#{@search_text}%\" OR body LIKE \"%#{@search_text}%\"")
+    end
+
+    if @search_start_at.present?
+      @blogs = @blogs.where("created_at >= '#{@search_start_at}'")
+    end
+
+    if @search_end_at.present?
+      @blogs = @blogs.where("created_at <= '#{@search_end_at}'")
+    end
   end
 
   def show
