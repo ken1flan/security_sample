28f5772d9f8766311b42cb6158ee98450232c405
ken1flan <ken1flan@gmail.com>
2019-02-21 07:48:17 +0900

    Change xss sample

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ end
 
 # javascript injection
 user = User.create(name: 'javascript injection', login_id: 'js_injection', email: 'js_injection@somewhere.com', password: 'password', self_introduction: Faker::Lorem.paragraph )
-user.create_measurement_tag(body: '<script>console.log(document.cookie);</script>')
+user.create_measurement_tag(body: '<script>alert(document.cookie);</script>')
 (rand(10) + 5).times.each do
   user.blogs.create(title: Faker::Lorem.words((rand(3) + 2)).join(' '), body: Faker::Lorem.paragraphs((rand(10) + 3)).join("\n"), status: Blog.statuses[:published])
 end
